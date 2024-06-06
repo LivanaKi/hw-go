f944f97edaa972ad620f64c306a3b17ef241fd13
LivanaKi <natzasha2015@gmail.com>
2024-06-06 18:18:48 +0300

    =(

--- book/nodeTree/nodeTree.go
+++ book/nodeTree/nodeTree.go
@@ -1,6 +1,11 @@
 // how work command
 // $ go tool compile -W nodeTree.go
 
+//AND
+
+//how work WebAssebly
+//$ GOOS=js GOARCH=wasm go build -o main.wasm nodeTree.go
+
 package main
 
 import(
---
+++ book/nodeTreeMore/go.mod
@@ -0,0 +1,3 @@
+module github.com/Users/natza/hw-go/book/nodeTreeMore
+
+go 1.19
